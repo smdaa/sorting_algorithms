e67085b6e8c8d7d4b0d1b84e476f83fb4867652e
smdaa <saadmdaa03@gmail.com>
2020-06-21 12:30:24 +0200

    insertion sort

---
+++ insertion.py
@@ -0,0 +1,13 @@
+def insertion_sort(A):
+	for j in range(1,len(A)):
+		key = A[j]
+		i = j-1
+		while i >= 0 and A[i] > key :
+			A[i + 1] = A[i]
+			i = i - 1
+		A[i + 1] = key
+
+A = [4, 3, 5, 2, 1]
+print(A)
+insertion_sort(A)
+print(A)
