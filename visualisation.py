43f54f7e4195c0debe98febc2397a127de078945
smdaa <saadmdaa03@gmail.com>
2020-06-22 23:26:50 +0200

    viz

--- visualisation.py
+++ visualisation.py
@@ -6,14 +6,15 @@ from quick import quick_sort
 import numpy as np
 from PIL import Image
 
-random_array = np.random.randint(0, 255, (100, 100, 3), dtype="uint8")
+random_array = np.random.randint(0, 255, (500, 500, 3), dtype="uint8")
 random_image = Image.fromarray(random_array)
 random_image.save("random_image.png")
 
 for i in range(random_array.shape[0]):
-    bubble_sort(random_array[:, i, 0])
-    bubble_sort(random_array[i, :, 1])
-    bubble_sort(random_array[i, :, 2])
+    n = random_array.shape[0]
+    quick_sort(random_array[:, i, 0], 0, n-1)
+    quick_sort(random_array[i, :, 1], 0, n-1)
+    quick_sort(random_array[i, :, 2], 0, n-1)
 
 sorted_image = Image.fromarray(random_array)
 sorted_image.save("sorted_image.png")
