3a14f1099b4de5b603de581babb206c009147a0e
smdaa <saadmdaa03@gmail.com>
2020-06-22 22:24:10 +0200

    tests

--- merge.py
+++ merge.py
@@ -1,19 +1,19 @@
 def merge(left_half, right_half):
-	res = []
-	i,j = 0,0
-	while i < len(left_half) and j < len(right_half):
-		if left_half[i] < right_half[j] :
-			res.append(left_half[i])
-			i += 1
-		else :
-			res.append(right_half[j])
-			j += 1
-	res += left_half[i:]
-	res += right_half[j:]
-	return res
+    res = []
+    i,j = 0,0
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j] :
+            res.append(left_half[i])
+            i += 1
+        else :
+            res.append(right_half[j])
+            j += 1
+    res += left_half[i:]
+    res += right_half[j:]
+    return res
 def merge_sort(A):
-	if len(A) <= 1 :
-		return A
-	n = len(A) // 2
-	return merge(merge_sort(A[:n]), merge_sort(A[n:]))
-	
+    if len(A) <= 1 :
+        return A
+    n = len(A) // 2
+    return merge(merge_sort(A[:n]), merge_sort(A[n:]))
+
---
+++ tests.py
@@ -0,0 +1,33 @@
+from bubble import bubble_sort
+from heap import heap_sort
+from insertion import insertion_sort
+from merge import merge_sort
+from quick import quick_sort
+import pytest
+
+B = [i for i in range(1, 101, 1)]
+
+def test_bubble():
+    A = [i for i in range(100, 0, -1)]
+    bubble_sort(A)
+    assert A == B
+
+def test_heap():
+    A = [i for i in range(100, 0, -1)]
+    heap_sort(A)
+    assert A == B
+
+def test_insertion():
+    A = [i for i in range(100, 0, -1)]
+    insertion_sort(A)
+    assert A == B
+
+def test_merge():
+    A = [i for i in range(100, 0, -1)]
+    A = merge_sort(A)
+    assert A == B
+
+def test_quick():
+    A = [i for i in range(100, 0, -1)]
+    quick_sort(A, 0, len(A) - 1)
+    assert A == B
