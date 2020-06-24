37161a44652e681f42bcda71cede4d2b08febbf5
smdaa <saadmdaa03@gmail.com>
2020-06-24 11:02:34 +0200

    tests

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from heap import heap_sort
 from insertion import insertion_sort
 from merge import merge_sort
 from quick import quick_sort
+from selection import selection_sort
 import pytest
 
 B = [i for i in range(1, 101, 1)]
@@ -30,4 +31,9 @@ def test_merge():
 def test_quick():
     A = [i for i in range(100, 0, -1)]
     quick_sort(A, 0, len(A) - 1)
+    assert A == B
+
+def test_selection():
+    A = [i for i in range(100, 0, -1)]
+    selection_sort(A)
     assert A == B
