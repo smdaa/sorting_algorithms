904c948042928e1634749ce89bec54649d263a82
smdaa <saadmdaa03@gmail.com>
2020-06-26 15:37:53 +0200

    insertion

---
+++ ocaml/insertion.ml
@@ -0,0 +1,13 @@
+let rec insert x a =
+  match a with 
+    [] -> [x]
+  | h :: t -> if x <= h then x :: h :: t 
+                        else h :: insert x t
+;;
+let rec insertion_sort a =
+    match a with 
+      [] -> []
+    | h :: t -> insert h (insertion_sort t)
+
+
+        
