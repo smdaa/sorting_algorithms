60f56ab6bd02014bb0dc7d2127705eb9d9b13df2
smdaa <saadmdaa03@gmail.com>
2020-06-26 15:30:38 +0200

    python

--- selection.py
+++
@@ -1,7 +0,0 @@
-def selection_sort(A):
-    for i in range(len(A)):
-        min = i
-        for j in range(i+1, len(A)):
-            if A[j] < A[min]:
-                min = j
-        A[min], A[i] = A[i], A[min]
